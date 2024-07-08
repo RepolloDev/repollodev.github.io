b17eb30ddf3d09e229c36124e54907d76675ae4a
repolloezzz <gabrielrepolloez@gmail.com>
2024-07-08 11:55:13 -0400

    🎊 First Commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://repollodev.github.io",
+  output: "static",
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [mdx(), sitemap(), tailwind(), icon({
+    iconDir: "src/assets/icons"
+  })],
+});
